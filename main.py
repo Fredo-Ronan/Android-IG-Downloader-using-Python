cc46a0ceac9382709796f61bdde0fb27c4825435
Fredo-Ronan <fredocode06@gmail.com>
2024-02-08 20:14:20 +0700

    fix some bugs again

--- main.py
+++ main.py
@@ -50,7 +50,7 @@ class InstaDownloaderApp(App):
         )
 
         header_label3 = Label(
-            text=f'version {self.latest_version}',
+            text=f'version {self.current_version}',
             font_size=18,
             color=[1, 1, 1, 1],  # White color
             halign='center',  # Center align the text
@@ -76,7 +76,7 @@ class InstaDownloaderApp(App):
             background_color=[0, 1, 1, 1],  # Green ish color
         )
         update_app_btn = Button(
-            text=f'Update to latest version {self.current_version}',
+            text=f'Update to latest version {self.latest_version}',
             font_size=24,
             on_press=self.update_app,
             background_color=[0, 1, 0.5, 1],
